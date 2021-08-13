2b3693f1d0918cdb4125fcfa1300a6820b44b0e3
soon-404 <tfpsoon@gmail.com>
2021-08-14 02:17:19 +0700

    cgange color

--- mylanding/src/App.js
+++ mylanding/src/App.js
@@ -3,28 +3,29 @@ import Fb from "./logo/fb";
 import Github from "./logo/github";
 function App() {
   return (
-    <div className='flex h-screen w-screen flex-col justify-center items-center bg-green-300'>
-      <text className='text-2xl md:text-6xl font-mono'>
+    <div className='flex h-screen w-screen flex-col justify-center items-center bg-yellow-400'>
+      <div className='my-20 w-40 md:w-100 h-40 md:h-100 rounded-full ring-4 md:ring-8 ring-blue-500 ring-offset-transparent bg-white'></div>
+      <text className='text-2xl smd:text-5xl lg:text-6xl font-mono'>
         Thanakorn Chancherngpanich
       </text>
       <div className='w-screen flex flex-row justify-items-center justify-center space-x-5 md:space-x-10'>
         <a
           href='https://www.facebook.com/profile.php?id=100014669342473'
-          className='w-12 md:w-24 h-12 md:h-24 '
+          className='w-14 md:w-24 h-14 md:h-24 '
         >
           <Fb></Fb>
         </a>
 
         <a
           href='https://www.instagram.com/_s00n__/'
-          className='w-12 md:w-24 h-12 md:h-24'
+          className='w-14 md:w-24 h-14 md:h-24'
         >
           <Ig></Ig>
         </a>
 
         <a
           href='https://github.com/soon-404'
-          className='w-12 md:w-24 h-12 md:h-24'
+          className='w-14 md:w-24 h-14 md:h-24'
         >
           <Github></Github>
         </a>
