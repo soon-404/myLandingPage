4810d4e64f4fa38bab7443dfbea73be8dd0d596f
soon-404 <tfpsoon@gmail.com>
2021-08-16 00:44:39 +0700

    V1.0

--- mylanding/src/App.js
+++ mylanding/src/App.js
@@ -3,24 +3,30 @@ import Fb from "./logo/fb";
 import Github from "./logo/github";
 import Line from "./logo/line";
 import Popline from "./components/PopLine";
+import Profile from "./images/soon.jpg";
 import { useState } from "react";
 function App() {
   const [linePop, setLinePop] = useState(false);
 
   return (
-    <div className='flex h-screen w-screen flex-col justify-center items-center bg-yellow-400'>
-      <main className='flex h-screen w-screen flex-col justify-center items-center bg-yellow-400'>
-        <div className='my-8 md:my-14 w-40 md:w-300 h-40 md:h-300 rounded-full ring-4 md:ring-8 ring-blue-500 ring-offset-transparent bg-white'></div>
-        <text className='mb-4 md:mb-5 text-2xl smd:text-5xl lg:text-6xl font-mono'>
+    <div className='flex h-screen w-screen flex-col justify-center items-center absolute inset-0'>
+      <div className='flex h-screen w-screen flex-col justify-center items-center bg-gradient-to-r from-blue-500 via-blue-400 to-green-400'>
+        <div className='my-8 md:my-14 w-40 md:w-300 h-40 md:h-300 rounded-full ring-4 md:ring-8 ring-white ring-offset-transparent bg-white'>
+          <img className=' rounded-full' src={Profile}></img>
+        </div>
+        <text className='mb-4 md:mb-5 text-2xl smd:text-5xl lg:text-6xl font-mono text-gray-100'>
           Thanakorn Chancherngpanich
         </text>
+        <text className='mb-4 md:mb-5 text-2xl smd:text-5xl lg:text-6xl font-mono text-gray-100'>
+          [ Soon ]
+        </text>
         <div className='w-screen flex flex-row justify-items-center justify-center space-x-5 md:space-x-10'>
-          <a
-            href='https://www.facebook.com/profile.php?id=100014669342473'
-            className='w-14 md:w-24 h-14 md:h-24 '
+          <div
+            className='w-14 md:w-24 h-14 md:h-24 cursor-pointer'
+            onClick={() => setLinePop(true)}
           >
-            <Fb />
-          </a>
+            <Line />
+          </div>
 
           <a
             href='https://www.instagram.com/_s00n__/'
@@ -36,14 +42,14 @@ function App() {
             <Github />
           </a>
 
-          <div
-            className='w-14 md:w-24 h-14 md:h-24 cursor-pointer'
-            onClick={() => setLinePop(true)}
+          <a
+            href='https://www.facebook.com/profile.php?id=100014669342473'
+            className='w-14 md:w-24 h-14 md:h-24 '
           >
-            <Line />
-          </div>
+            <Fb />
+          </a>
         </div>
-      </main>
+      </div>
       <Popline trigger={linePop} setTrigger={setLinePop}></Popline>
     </div>
   );
