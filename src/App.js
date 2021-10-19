99ae2ad96a39e7f03ccb8882b1aa7c17ecfdb5cb
soon-404 <tfpsoon@gmail.com>
2021-10-19 20:59:51 +0700

    delete sound

--- src/App.js
+++ src/App.js
@@ -5,19 +5,14 @@ import Line from "./logo/line";
 import Popline from "./components/PopLine";
 import Profile from "./images/soon.jpg";
 import { useState } from "react";
-import useSound from "use-sound";
-import test from "./Sounds/test.mp3";
+
 function App() {
   const [linePop, setLinePop] = useState(false);
-  const [play, { stop }] = useSound(test, { volume: 0.5 });
   return (
     <div className='flex h-screen w-screen flex-col justify-center items-center absolute inset-0'>
-      <div
-        className='flex h-screen w-screen flex-col justify-center items-center bg-gradient-to-r from-blue-500 via-blue-400 to-green-400'
-        onClick={stop}
-      >
+      <div className='flex h-screen w-screen flex-col justify-center items-center bg-gradient-to-r from-blue-500 via-blue-400 to-green-400'>
         <div className='my-8 md:my-14 w-40 md:w-300 h-40 md:h-300 rounded-full ring-4 md:ring-8 ring-white ring-offset-transparent bg-white'>
-          <img className=' rounded-full' src={Profile} onClick={play}></img>
+          <img className=' rounded-full' src={Profile}></img>
         </div>
         <text className='mb-4 md:mb-5 text-2xl smd:text-5xl lg:text-6xl font-mono text-gray-100'>
           Thanakorn Chancherngpanich
@@ -34,7 +29,7 @@ function App() {
           </div>
 
           <a
-            href='https://www.instagram.com/_s00n__/'
+            href='https://www.instagram.com/_soon404/'
             className='w-14 md:w-24 h-14 md:h-24'
           >
             <Ig />
